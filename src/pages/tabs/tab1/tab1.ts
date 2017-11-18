d3994e67685615ea2845890715accc12336cc4d1
Ravi G <ravi.g@spurtreetech.com>
2017-11-18 08:49:13 +0530

    Done tab changes

--- src/pages/tabs/tab1/tab1.ts
+++ src/pages/tabs/tab1/tab1.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams,ModalController, ActionSheetController } from 'ionic-angular';
+import {EventItem,FavItem} from '../../../models/event-item/event-item.interface';
+import {AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import {MyFav} from '../../../models/myfav';
+import { EventDetailsPage } from '../../event-details/event-details';
+import { SuperTabsController } from 'ionic2-super-tabs';      
 
 
 
@@ -8,12 +13,133 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'tab1.html',
 })
 export class Tab1Page {
+public isFavourite: boolean = false;
+  myFav = {} as MyFav;
+  myFavst : any = [];
+  newList:any =[];
+  
+  public today = new Date().getTime();
+  public getDate = new Date("2016-08-20").getTime(); 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public currentUser:any = JSON.parse(localStorage.getItem("currentUser"));
+  public myFavs:any = JSON.parse(localStorage.getItem("currentUserMyFavs"));
+  public dummyArrs : any;
+  eventListRef$: FirebaseListObservable<EventItem[]>
+  eventFavRef$: FirebaseListObservable<FavItem[]>
+  public oneObj:any;
+  public twoObj:any =[];
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private database:AngularFireDatabase, 
+    public actionsheetCtrl: ActionSheetController,
+    public db:AngularFireDatabase,
+    private superTabsCtrl: SuperTabsController,
+    public modalCtrl: ModalController
+    ) {
+      
+      
+      if(this.getDate >= this.today){
+        //console.log("upcoming")
+      }else{
+        //console.log("past")
+      }
+
+    //this.superTabsCtrl.showToolbar(true);
+    this.eventListRef$ = this.database.list('Event-List');
+    this.eventFavRef$ = this.database.list(`profile/${this.currentUser.uid}/myfavs/`);
+
+    this.eventListRef$.subscribe(data=>{
+      //console.log(data)
+      this.newList = data;
+       this.eventFavRef$.subscribe(data=>{
+        //console.log(data)
+        this.twoObj=data;
+        for(let i=0;i<this.newList.length;i++){
+          for(let j=0;j<this.twoObj.length;j++){
+            if(this.newList[i].$key == this.twoObj[j].eventId){
+              this.newList[i].favourite = true;
+            }
+          }
+        }
+      });
+    });
+   
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Tab1Page');
+upcomingEvent(dates){
+  let item = new Date(dates).getTime();
+  if(item >= this.today){
+    return true;
+  }else{
+    return false;
+  }  
+};
+pastEvent(dates){
+  let item = new Date(dates).getTime();
+  if(item <= this.today){
+    return true;
+  }else{
+    return false;
+  }  
+};
+
+ionViewDidLoad() {
+    //console.log('ionViewDidLoad EventDetailsPage');
+    this.superTabsCtrl.showToolbar(true);
+  }
+  
+
+  isFavouriteFun(item){    
+    item.favourite = !item.favourite;
+      if(item.favourite){
+        this.db.object(`profile/${this.currentUser.uid}/myfavs/${item.$key}/`).set({"eventId":item.$key});        
+      }else{
+        this.db.list(`profile/${this.currentUser.uid}/myfavs`).remove(item.$key);
+      }        
+  }
+  goToEventDetails(eventItem:EventItem){
+    //console.log(eventItem.$key)
+    //this.navCtrl.push(EventDetailsPage,{eventId: eventItem.$key})
+    let modal = this.modalCtrl.create(EventDetailsPage,{eventId: eventItem.$key});
+    modal.present();
+  }
+
+  openMenu(eventItem:EventItem) {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: `${eventItem.eventName}`,
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () => {
+            //console.log('Delete clicked');
+            this.eventListRef$.remove(eventItem.$key);
+          }
+        },
+        {
+          text: 'Edit',
+          handler: () => {
+            //this.navCtrl.push(EditEventPage,{eventId: eventItem.$key})
+          }
+        },
+        {
+          text: 'Favorite',
+          handler: () => {
+            //console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', // will always sort to be on the bottom
+          handler: () => {
+            //console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
--- src/pages/tabs/tab2/tab2.ts
+++ src/pages/tabs/tab2/tab2.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
+import { NavController, NavParams,ModalController,ActionSheetController } from 'ionic-angular';
+import {EventItem,FavItem} from '../../../models/event-item/event-item.interface';
+import {AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import {MyFav} from '../../../models/myfav';
+import { EventDetailsPage } from '../../event-details/event-details';
+import { SuperTabsController } from 'ionic2-super-tabs';      
+
 
 
 @Component({
@@ -8,85 +13,133 @@ import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
   templateUrl: 'tab2.html',
 })
 export class Tab2Page {
-  public videoURL:any;
-  public carousel: any = [{
-    eventTitle:"H.K Prakash Cup 2017",
-    eventDate:"09-Sep-2017",
-    videos:[{
-      id:"Pwy4jG2ExOk",
-      title:"Semi Final",
-      players:"Amar Jith & Yeswanth Vs Abhishek & Partner"
-    },{
-      id:"SDqrOtFhVPM",
-      title:"Semi Final",
-      players:"Amar Jith & Yeswanth Vs Abhishek & Partner"
-    },{
-      id:"IOmta8T_HnY",
-      title:"Semi Final",
-      players:"Amar Jith & Yeswanth Vs Abhishek & Partner"
-    },{
-      id:"p-hdIRjDx3Q",
-      title:"Semi Final",
-      players:"Amar Jith & Yeswanth Vs Abhishek & Partner"
-    },{
-      id:"_zxxJgkPhvU",
-      title:"Semi Final",
-      players:"Amar Jith & Yeswanth Vs Abhishek & Partner"
-    },{
-      id:"dgZxzv8SDoQ",
-      title:"Semi Final",
-      players:"Amar Jith & Yeswanth Vs Abhishek & Partner"
-    }]
-  },{
-    eventTitle:"Play Extrem Cup 2017",
-    eventDate:"10-Aug-2017",
-    videos:[{
-      id:"Pwy4jG2ExOk",
-      title:"Semi Final",
-      players:"Amar Jith & Yeswanth Vs Abhishek & Partner"
-    },{
-      id:"SDqrOtFhVPM",
-      title:"Semi Final",
-      players:"Amar Jith & Yeswanth Vs Abhishek & Partner"
-    },{
-      id:"IOmta8T_HnY",
-      title:"Semi Final",
-      players:"Amar Jith & Yeswanth Vs Abhishek & Partner"
-    },{
-      id:"p-hdIRjDx3Q",
-      title:"Semi Final",
-      players:"Amar Jith & Yeswanth Vs Abhishek & Partner"
-    },{
-      id:"_zxxJgkPhvU",
-      title:"Semi Final",
-      players:"Amar Jith & Yeswanth Vs Abhishek & Partner"
-    },{
-      id:"dgZxzv8SDoQ",
-      title:"Semi Final",
-      players:"Amar Jith & Yeswanth Vs Abhishek & Partner"
-    }]
-  }];
-  //public images:any = ['1.jpg','2.jpg','3.jpg','4.jpg']
-  constructor(public navCtrl: NavController, public navParams: NavParams, private youtube: YoutubeVideoPlayer) {
-    //this.selectedItem = navParams.get('item');
-    let id = ["SDqrOtFhVPM","Pwy4jG2ExOk","IOmta8T_HnY","p-hdIRjDx3Q","_zxxJgkPhvU","dgZxzv8SDoQ"];
-    this.videoURL = 'http://img.youtube.com/vi/' + id + '/default.jpg';
+public isFavourite: boolean = false;
+  myFav = {} as MyFav;
+  myFavst : any = [];
+  newList:any =[];
+  
+  public today = new Date().getTime();
+  public getDate = new Date("2016-08-20").getTime(); 
+
+  public currentUser:any = JSON.parse(localStorage.getItem("currentUser"));
+  public myFavs:any = JSON.parse(localStorage.getItem("currentUserMyFavs"));
+  public dummyArrs : any;
+  eventListRef$: FirebaseListObservable<EventItem[]>
+  eventFavRef$: FirebaseListObservable<FavItem[]>
+  public oneObj:any;
+  public twoObj:any =[];
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private database:AngularFireDatabase, 
+    public actionsheetCtrl: ActionSheetController,
+    public db:AngularFireDatabase,
+    private superTabsCtrl: SuperTabsController,
+    public modalCtrl: ModalController
+    ) {
+      
+      
+      if(this.getDate >= this.today){
+        //console.log("upcoming")
+      }else{
+        //console.log("past")
+      }
+
+    //this.superTabsCtrl.showToolbar(true);
+    this.eventListRef$ = this.database.list('Event-List');
+    this.eventFavRef$ = this.database.list(`profile/${this.currentUser.uid}/myfavs/`);
+
+    this.eventListRef$.subscribe(data=>{
+      //console.log(data)
+      this.newList = data;
+       this.eventFavRef$.subscribe(data=>{
+        //console.log(data)
+        this.twoObj=data;
+        for(let i=0;i<this.newList.length;i++){
+          for(let j=0;j<this.twoObj.length;j++){
+            if(this.newList[i].$key == this.twoObj[j].eventId){
+              this.newList[i].favourite = true;
+            }
+          }
+        }
+      });
+    });
+   
   }
 
-  ionViewDidLoad() {
-    //console.log('ionViewDidLoad Tab2Page');
-    //url = 'http://img.youtube.com/vi/' + thumb + '/default.jpg';
- 
-    
+upcomingEvent(dates){
+  let item = new Date(dates).getTime();
+  if(item >= this.today){
+    return true;
+  }else{
+    return false;
+  }  
+};
+pastEvent(dates){
+  let item = new Date(dates).getTime();
+  if(item <= this.today){
+    return true;
+  }else{
+    return false;
+  }  
+};
+
+ionViewDidLoad() {
+    //console.log('ionViewDidLoad EventDetailsPage');
+    this.superTabsCtrl.showToolbar(true);
   }
-  async openVideo(){
-    try{
-      await this.youtube.openVideo('SDqrOtFhVPM');
-    }
-    catch(e){
-      console.error(e);
-    }
-    
+  
+
+  isFavouriteFun(item){    
+    item.favourite = !item.favourite;
+      if(item.favourite){
+        this.db.object(`profile/${this.currentUser.uid}/myfavs/${item.$key}/`).set({"eventId":item.$key});        
+      }else{
+        this.db.list(`profile/${this.currentUser.uid}/myfavs`).remove(item.$key);
+      }        
+  }
+  goToEventDetails(eventItem:EventItem){
+    //console.log(eventItem.$key)
+    //this.navCtrl.push(EventDetailsPage,{eventId: eventItem.$key})
+    let modal = this.modalCtrl.create(EventDetailsPage,{eventId: eventItem.$key});
+    modal.present();
+  }
+
+  openMenu(eventItem:EventItem) {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: `${eventItem.eventName}`,
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () => {
+            //console.log('Delete clicked');
+            this.eventListRef$.remove(eventItem.$key);
+          }
+        },
+        {
+          text: 'Edit',
+          handler: () => {
+            //this.navCtrl.push(EditEventPage,{eventId: eventItem.$key})
+          }
+        },
+        {
+          text: 'Favorite',
+          handler: () => {
+            //console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', // will always sort to be on the bottom
+          handler: () => {
+            //console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
